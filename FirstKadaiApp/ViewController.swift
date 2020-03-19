5a5b6587e68b8e48c02bc2e9d90bc25d07fd523e
coooger <cooogerx@gmail.com>
2020-03-19 15:41:45 +0900

    はじめてのアプリ

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by Mr.A on 2020/03/19.
+//  Copyright © 2020 coooger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
